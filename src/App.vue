c1ae56e214a48b953d262946954b690988766383
zjtakato <zjtakato@gmail.com>
2024-03-22 10:54:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { h, render, resolveComponent } from 'vue';
+
+function createNotify() {
+  const container = document.createElement('div');
+  const vnode = h({
+    render() {
+      const ElBtn = resolveComponent('el-button');
+      console.log(ElBtn); // is 'el-button' string, not vnode
+      return h(ElBtn, { type: 'primary' });
+    },
+  });
+  render(vnode, container);
+}
+</script>
+
+<template>
+  <el-button @click="createNotify">create notify</el-button>
+</template>
+
+<style scoped></style>
